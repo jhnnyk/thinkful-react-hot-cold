37ef522fb14032367d781d4e5227ca539b179f07
John Knotts <jhnnyk@gmail.com>
2017-11-05 21:10:02 -0700

    hello world

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import HotorCold from './components/hot-or-cold'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<HotorCold />, document.getElementById('root'))
+registerServiceWorker()
