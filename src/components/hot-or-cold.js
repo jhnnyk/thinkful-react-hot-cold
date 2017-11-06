78f69e80240e22f9dd1443f0e039886249c89e41
John Knotts <jhnnyk@gmail.com>
2017-11-06 11:57:38 -0700

    remove bulma 😕

--- src/components/hot-or-cold.js
+++ src/components/hot-or-cold.js
@@ -4,7 +4,7 @@ class HotorCold extends Component {
   render () {
     return (
       <div>
-        <h1 className='title'>Hello world!</h1>
+        <h1>Hello world!</h1>
       </div>
     )
   }
